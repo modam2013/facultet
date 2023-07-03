c31a06c39a5b3e9a3a83879982309de3bedb6dc3
Аня <grigorevna.antoninka@mail.ru>
2023-07-03 20:46:20 +0700

    Initial commit

---
+++ src/main/java/com/example/facultet/FacultetApplication.java
@@ -0,0 +1,13 @@
+package com.example.facultet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/facultet/FacultetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facultet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
